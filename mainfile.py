15125ce09a1baea4d00d24734bd2e0dc7f9d532e
devilAj47 <52520467+devilAj47@users.noreply.github.com>
2021-01-06 17:01:56 +0530

    Update mainfile.py

--- mainfile.py
+++ mainfile.py
@@ -405,4 +405,4 @@ def video_feed():
                  mimetype='multipart/x-mixed-replace; boundary=frame')
 
 if __name__ == '__main__':
-    app.run(host='127.0.0.1', debug=True,port="5000")
+    app.run(debug=True)
